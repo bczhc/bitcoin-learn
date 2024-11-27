59c1d2f63c0eb33022e565949773b440d21eaef6
Zhai Can <bczhc0@126.com>
2024-11-28 06:06:47 +0800

    update

--- src/tools/vanity_address.rs
+++ src/tools/vanity_address.rs
@@ -1,5 +1,5 @@
-use bitcoin::secp256k1::Secp256k1;
-use bitcoin::{Address, NetworkKind, PrivateKey, PubkeyHash};
+use bitcoin::secp256k1::{Secp256k1, SecretKey};
+use bitcoin::{Address, NetworkKind, PrivateKey};
 use rand::rngs::OsRng;
 use rand::RngCore;
 use std::thread::scope;
@@ -7,39 +7,45 @@ use std::{fmt, mem};
 
 type Key = (u128, u128);
 
-fn as_key_array(key: &mut Key) -> &mut [u8; 32] {
-    unsafe { mem::transmute(key) }
+#[inline(always)]
+fn increase_sk(secret_key: &mut SecretKey) {
+    unsafe {
+        let num: &mut (u128, u128) = mem::transmute(secret_key);
+        num.0 += 1;
+    }
 }
 
 fn main() {
     assert_eq!(size_of::<Key>(), 256 / 8);
+    assert_eq!(size_of::<SecretKey>(), 256 / 8);
 
     let jobs = num_cpus::get();
-    let start = "1Cafe";
+    let start = "1Arch";
     scope(|s| {
         for _ in 0..jobs {
-            s.spawn(|| {
-                let mut initial = (0_u128, 0_u128);
-                OsRng.fill_bytes(as_key_array(&mut initial));
+            s.spawn(|| unsafe {
+                let mut initial = [0_u8; 32];
+                OsRng.fill_bytes(&mut initial);
+                let sk: SecretKey = mem::transmute_copy(&initial);
+                let mut prk = PrivateKey {
+                    compressed: true,
+                    network: NetworkKind::Main,
+                    inner: sk,
+                };
                 let secp: Secp256k1<_> = Default::default();
                 let mut addr_buf = String::with_capacity(50);
                 loop {
-                    // TODO: use uncheck
-                    unsafe {
-                        let pk =
-                            PrivateKey::from_slice(as_key_array(&mut initial), NetworkKind::Main)
-                                .unwrap_unchecked();
-                        let addr = Address::p2pkh(pk.public_key(&secp), NetworkKind::Main);
-                        use fmt::Write;
-                        unsafe {
-                            addr_buf.clear();
-                            write!(&mut addr_buf, "{}", addr).unwrap_unchecked();
-                            if addr_buf.starts_with(start) {
-                                println!("{} {}", addr_buf, pk.to_wif())
-                            }
-                        }
-                        initial.0 += 1;
+                    let addr = Address::p2pkh(prk.public_key(&secp), NetworkKind::Main);
+                    use fmt::Write;
+                    addr_buf.clear();
+                    write!(&mut addr_buf, "{}", addr).unwrap_unchecked();
+                    if addr_buf.starts_with(start) {
+                        // validate secret key
+                        SecretKey::from_slice(prk.inner.as_ref()).unwrap();
+                        let address = addr.to_string();
+                        println!("{} {}", address, prk.to_wif())
                     }
+                    increase_sk(&mut prk.inner);
                 }
             });
         }
